48e1b8a48d3c2b7b399fe79117bef000accdbffa
pako-g <2630798008pas>
2018-03-22 13:41:17 +0100

    add ContentChild and ContentChildren

--- ex5-components/src/app/app.component.ts
+++ ex5-components/src/app/app.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, ContentChild, ContentChildren, style} from '@angular/core';
+
+@Component({
+  selector: 'Person',
+  template: `<div>&nbsp;-&nbsp;<ng-content></ng-content></div>`,
+  styles: ['']
+})
+export class Person {
+}
+
 
 @Component({
   selector: 'Paragraph',
-  template: '<p><ng-content></ng-content></p>'
+  template: `
+  <div>
+    <b>{{title.nativeElement.innerHTML}}</b>
+    <p><ng-content></ng-content></p>
+    <p *ngIf="people">Number of People: {{people.length}}</p>
+  </div>`,
   styles: ['p { border: 1px solid #c0c0c0; padding: 10px }']
 })
-export class Paragraph {}
-
+export class Paragraph {
+  @ContentChild('title') title;
+  @ContentChildren(Person) people;
+}
 
 @Component({
   selector: 'app-root',
   template: `
     <p>
-      <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </Paragraph>
-      <Paragraph>Praesent eget ornare neque, vel consectetur eros. </Paragraph> 
+      <Paragraph>
+        <title #title>Paragraph1</title>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        <Person>Albertus Falx</Person>
+        <Person>Godefridus Turpilius</Person>
+        <Person>Demipho Renatus</Person>
+      </Paragraph>
+      <Paragraph>
+        <title #title>Paragraph1</title>
+        Praesent eget ornare neque, vel consectetur eros.
+        <Person>Hanno Grumio</Person>
+        <Person>Lycus Auxilius</Person>
+      </Paragraph>
     </p>
   `,
   styleUrls: ['./app.component.css']
--- ex5-components/src/app/app.module.ts
+++ ex5-components/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent, Paragraph } from './app.component';
+import { AppComponent, Paragraph, Person } from './app.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Paragraph
+    Paragraph,
+    Person
   ],
   imports: [
     BrowserModule
